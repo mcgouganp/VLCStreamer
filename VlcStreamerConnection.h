28c086a65a65eb7bdc95fc5f3e755165b778ca14
Paul McGougan <paul@gallandro.net>
2012-06-14 13:42:00 +1000

    Use VlcStreamerFileSystem and change to QString
    Removed functions that are now handled by VlcStreamerFileSystem.
    Whereever it was more appropriate, replace QByteArray with QString.

--- VlcStreamerConnection.h
+++ VlcStreamerConnection.h
@@ -23,54 +23,43 @@ private:
 		_Request()	{ Clear(); }
 		void Clear();
 
-		bool		AddLine(const QByteArray &line);
+		bool		AddLine(const QString &line);
 		bool		IsComplete() const				{ return _complete; }
-		QByteArray	Method() const					{ return _method; }
+		QString		Method() const					{ return _method; }
 		const QUrl	&Url() const					{ return _url; }
-		QByteArray	Version() const					{ return _version; }
-		QByteArray	Value(const QByteArray &name)	{ return _headers.value(name); }
+		QString		Version() const					{ return _version; }
+		QString		Value(const QString &name)		{ return _headers.value(name); }
 
 	private:
-		bool							_complete;
-		QByteArray						_method;
-		QUrl							_url;
-		QByteArray						_version;
-		QMap<QByteArray, QByteArray>	_headers;
+		bool					_complete;
+		QString					_method;
+		QUrl					_url;
+		QString					_version;
+		QMap<QString, QString>	_headers;
 	};
 
 	class _Response {
 	public:
 		_Response();
 
-		_Response	&Version(const QByteArray &version)							{ _version = version; return *this; }
-		_Response	&Status(unsigned status)									{ _status = status; return *this; }
-		_Response	&ResponsePhrase(const QByteArray &phrase)					{ _responsePhrase = phrase; return *this; }
-		_Response	&Header(const QByteArray &name, const QByteArray &value)	{ _headers.append(qMakePair(name, value)); return *this; }
-		_Response	&Data(const QByteArray &data, const QByteArray &type = "text/html");
+		_Response	&Version(const QString &version)					{ _version = version; return *this; }
+		_Response	&Status(unsigned status)							{ _status = status; return *this; }
+		_Response	&ResponsePhrase(const QString &phrase)				{ _responsePhrase = phrase; return *this; }
+		_Response	&Header(const QString &name, const QString &value)	{ _headers.append(qMakePair(name, value)); return *this; }
+		_Response	&Data(const QByteArray &data, const QString &type = "text/html");
 
 		QByteArray	ToByteArray();
 
 	private:
-		QByteArray								_version;
-		unsigned								_status;
-		QByteArray								_responsePhrase;
-		QVector<QPair<QByteArray, QByteArray> >	_headers;
-		QByteArray								_data;
+		QString								_version;
+		unsigned							_status;
+		QString								_responsePhrase;
+		QVector<QPair<QString, QString> >	_headers;
+		QByteArray							_data;
 	};
-
-	struct _VideoInfo {
-		QByteArray	params;
-		QByteArray	name;
-		QByteArray	status;
-	};
-
-	static void		_DeleteDirectory(QDir &name);
-	static QString	_ServerId();
 
 	void	_Respond();
 	
-	QVector<_VideoInfo>	_GetConvertedVideoInfo() const;
-
 	QTimer		_readTimer;
 	_Request	_request;
 };
